c53e1a5be1f2df30d4f694c99f913ad760f0c018
unknown <hamdiiptv6@gmail.com>
2022-07-13 13:30:33 +0100

    eureka server pour tous les microservice

--- clients/src/main/java/com/microservices/clientui/ClientuiApplication.java
+++ clients/src/main/java/com/microservices/clientui/ClientuiApplication.java
@@ -2,11 +2,13 @@ package com.microservices.clientui;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 
 @EnableFeignClients("com.microservices.clientui")
+@EnableDiscoveryClient
 public class ClientuiApplication {
 
 	public static void main(String[] args) {
---
+++ config-zull/src/test/java/com/microservices/configzull/ConfigZullApplicationTests.java
@@ -0,0 +1,11 @@
+package com.microservices.configzull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigZullApplicationTests {
+
+
+
+}
